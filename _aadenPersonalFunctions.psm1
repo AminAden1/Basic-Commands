3628c25439a192c5965c129df345b2bca2b32c14
Amin Aden <aminaden@Amins-MacBook-Air-2.local>
2023-05-12 22:38:59 -0500

    complete file

--- _aadenPersonalFunctions.psm1
+++ _aadenPersonalFunctions.psm1
@@ -6,3 +6,19 @@ function p {
     param($computername)
     return (test-connection $computername -count 1 -quiet)
 }
+#-----------------------------------------------
+#function Name : Get-LoggedIn
+# Return the current logged-In user of a remote machine.
+#-----------------------------------------------
+function Get-LoggedIn {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True)]
+        [string[]]$computername
+    )
+    foreach ($pc in $computername){
+        $logged_in = ( gmwi win32_computersystem -COMPUTER $pc).username 
+        $name = $logged_in.split('\')[1]
+        "{0}: {1}" -f $pc, $name
+    }
+}
